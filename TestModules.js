01aa2efd756598db0fcc0d97b453bfed9c2d10f1
Autsada Wiriya <aut0935261784@gmail.com>
2022-12-31 13:57:01 +0700

    Add

---
+++ TestModules.js
@@ -0,0 +1,5 @@
+const utill = require(`./modules/mymodules.js`)
+const getCurrentTime = require(`./modules/mymodules.js`).getCurrentTime
+
+console.log(getCurrentTime())
+console.log(utill.add(100, 5))
