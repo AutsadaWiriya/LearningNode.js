6e2c0a2f5ecea3b217a55506617e39771d4f7cfc
AutsadaWiriya <aut0935261784@gmail.com>
2023-01-02 18:36:30 +0700

    Add

--- TestModules.js
+++ TestModules.js
@@ -3,5 +3,3 @@ const getCurrentTime = require(`./modules/mymodules.js`).getCurrentTime
 
 console.log(getCurrentTime())
 console.log(utill.add(100, 5))
-console.log(utill.add(100, 5))
-console.log(utill.add(100, 5))
